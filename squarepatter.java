3265ea625d9bf98d5773732aa79cb2fa9eaec7ea
ayush rajput <ayushrajput16102005@gmail.com>
2024-07-26 18:00:17 +0530

    square pattern

---
+++ squarepatter.java
@@ -0,0 +1,16 @@
+public class squarepatter {
+    public static void main(String args[]) {
+        int n = 5; //number of rows
+        int m = 5; //number of coloumns
+
+        for(int i=1;i<=n;i++) {
+            //loop making rows
+            for(int j=1;j<=m;j++) {
+                //loop printing element in cell
+                System.out.print("*");
+            }
+            System.out.println(); // making sure next row will be print in next line
+        }
+        
+    }    
+}
